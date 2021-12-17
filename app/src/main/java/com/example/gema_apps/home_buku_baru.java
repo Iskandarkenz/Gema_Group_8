b6ed11b8cf6e6209efdbe07c23dd7ee7c088e7b7
AnggiAprianda <anggiaprianda2000@gmail.com>
2021-12-17 21:05:47 +0700

    Punya Anggi >

---
+++ app/src/main/java/com/example/gema_apps/home_buku_baru.java
@@ -0,0 +1,14 @@
+package com.example.gema_apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home_buku_baru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_buku_baru);
+    }
+}
---
+++ app/src/main/java/com/example/gema_apps/home_semuabuku.java
@@ -0,0 +1,14 @@
+package com.example.gema_apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home_semuabuku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_semua_buku);
+    }
+}
