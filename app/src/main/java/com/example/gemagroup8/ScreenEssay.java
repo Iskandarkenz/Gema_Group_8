f4e2a1e58831b17f43adb77c88cd6e34b79110ac
RafikaFadilaDaulay <rafikafadila15@gmail.com>
2021-12-26 19:04:30 +0700

    ScreenMasukProfil, ScreenEssay, ScreenSelesaiQuiz, ScreenRating.

---
+++ app/src/main/java/com/example/gemagroup8/ScreenEssay.java
@@ -0,0 +1,14 @@
+package com.example.gemagroup8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenEssay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_essay);
+    }
+}
---
+++ app/src/main/java/com/example/gemagroup8/ScreenSelesaiQuiz.java
@@ -0,0 +1,14 @@
+package com.example.gemagroup8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenSelesaiQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_selesai_quiz);
+    }
+}
